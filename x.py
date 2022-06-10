d8d4de03788f142e767b7273a6b46497e8717fa7
Boyao SONG <85090456+akaigua@users.noreply.github.com>
2022-06-10 15:06:43 +0800

    Updated Blog

--- x.py
+++ x.py
@@ -23,7 +23,7 @@ def parse_md(files: List[str]) -> str:
     for file in files:
         s += '### [' + file.split('/')[-1] + f']({file}) \n'
         s += f'Last Modified: {datetime.fromtimestamp(os.path.getmtime(file)).strftime("%m-%d-%y")}' + '<br>'
-        s += f'View in [Github]({config.ORIGIN + "/blob/main/" + file}) for better experience if this document contains Latex Formula\n'
+        s += f'[View in Github]({config.ORIGIN + "/blob/main/" + file}) for better experience if this document contains Latex Formula\n'
         s += '#### Preview: '+'\n\n'
         with open(file, mode='r') as f:
             s += unmark(f.read()[:config.DESC_LENGTH]) + '\n'
