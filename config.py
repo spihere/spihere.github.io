2fbc4b8dbd52e241811f08659618cc3ed0c7956f
Boyao SONG <85090456+akaigua@users.noreply.github.com>
2022-06-10 15:07:58 +0800

    Updated Blog

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TITLE = "SBI CodeSpace"
 UNDER_TITLE_TEXT = None
-DESC_LENGTH = 100
+DESC_LENGTH = 200
 FOOTER = "[Parser](https://github.com/sbihere/) built by SPI using python with love"
 ORIGIN = "https://github.com/spihere/spihere.github.io"
